6e60b96ba080f410f05553501deb5b9513508d51
Khushali Desai <khushali.desai@sjsu.edu>
2014-12-27 00:55:01 -0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,10 +1,10 @@
 import twilioRestClient
-account_sid=“ACc5899cfa57cdf1054f654b65277b98e8”
-auth_token=“”
+account_sid=“your sid pls”
+auth_token=“your token pls”
 client=TwilioRestClient(account_sid,auth_token)
-client=TwilioRestClient(account_sid,auth_token)
+
 message=client.sms.message.create(
-body=“Hello”
-to=“+1”
-from=”+1513”)
+body=“Hello”,
+to=“+1”,
+from_=”+1513”)
 print message.sid
