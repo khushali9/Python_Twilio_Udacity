15b51ba07973958b0d9955bb2f361490b3c2444d
Khushali Desai <khushali.desai@sjsu.edu>
2014-12-27 00:56:09 -0800

    Update send_message.py
    twilio no and authentication hidden :)

--- send_message.py
+++ send_message.py
@@ -1,10 +1,6 @@
-import twilioRestClient
-account_sid=“your sid pls”
-auth_token=“your token pls”
+from twilio.rest import TwilioRestClient
+account_sid="***"
+auth_token="***"
 client=TwilioRestClient(account_sid,auth_token)
-
-message=client.sms.message.create(
-body=“Hello”,
-to=“+1”,
-from_=”+1513”)
+message=client.sms.messages.create(body="Hello Khushali", to="+1your_no" ,from_="+1twilio_no")
 print message.sid
